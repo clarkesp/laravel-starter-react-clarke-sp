c22a9a057bcf01c65257798277ca25205f886ff0
clarkesp <clarke.noddie.io@gmail.com>
2025-11-08 12:50:36 -0300

    First reboot

---
+++ app/Admin/Http/Controllers/Controller.php
@@ -0,0 +1,8 @@
+<?php
+
+namespace App\Admin\Http\Controllers;
+
+abstract class Controller
+{
+    //
+}
--- bootstrap/app.php
+++ bootstrap/app.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Admin\Http\Middleware\HandleInertiaRequests as AdminHandleInertiaRequests;
+use App\Admin\Http\Middleware\HandleAppearance as AdminHandleAppearance;
 use App\Http\Middleware\HandleAppearance;
 use App\Http\Middleware\HandleInertiaRequests;
 use Illuminate\Foundation\Application;
@@ -12,14 +14,25 @@ return Application::configure(basePath: dirname(__DIR__))
         web: __DIR__.'/../routes/web.php',
         commands: __DIR__.'/../routes/console.php',
         health: '/up',
+        then: function () {
+            Route::middleware('web')
+                ->group(base_path('routes/admin.php'));
+        }
     )
     ->withMiddleware(function (Middleware $middleware): void {
         $middleware->encryptCookies(except: ['appearance', 'sidebar_state']);
 
+        // Regular user middleware
         $middleware->web(append: [
             HandleAppearance::class,
             HandleInertiaRequests::class,
             AddLinkHeadersForPreloadedAssets::class,
+        ]);
+        
+        // Admin-specific middleware (applied to admin routes)
+        $middleware->group('admin', [
+            AdminHandleAppearance::class,
+            AdminHandleInertiaRequests::class,
         ]);
     })
     ->withExceptions(function (Exceptions $exceptions): void {
